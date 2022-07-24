8d766f66085ae560200eaac56be76209f21744c7
Konstantin S <turnever@gmail.com>
2022-07-25 02:21:29 +1000

    feat boost: link boost.Log by cmake & test it by using trivial log

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-int main(int argc, char const* const argv[], char const* const envp[]) {
-    return 0;
+#include <boost/log/trivial.hpp>
+
+int main(int argc, char const *const argv[], char const *const envp[]) {
+  BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+  BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+  BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+  BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+  BOOST_LOG_TRIVIAL(error) << "An error severity message";
+  BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+
+  return 0;
 }
